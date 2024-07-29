bf0cc05df8f998a91a661545986d3d27c9f5121f
Szymon Piątek <szymon.piatek@pisf.pl>
2024-07-29 18:53:30 +0200

    Fix place cards

--- game/cards/place_cards.py
+++ game/cards/place_cards.py
@@ -1,8 +1,10 @@
 place_cards = {
     "Ulica pokątna": {
-        "max_death_eater": 4
+        "max_death_eater": 4,
+        "take_card": 1,
     },
     "Zwierciadło Ain Eingarp": {
-        "max_death_eater": 4
+        "max_death_eater": 4,
+        "take_card": 1,
     },
 }
